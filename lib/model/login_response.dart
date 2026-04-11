5d00288e5fb5136d45fc975f0524ecca462d19ec
madelynlato27-prog <madelynlato27@gmail.com>
2026-04-11 08:38:07 +0800

    new update

--- lib/model/login_response.dart
+++ lib/model/login_response.dart
@@ -1,22 +1,21 @@
 class LoginResponse {
   final int status;
   final String message;
-  final LoginData data;
-  final String token;
+  final LoginData? data;
 
   LoginResponse({
     required this.status,
     required this.message,
-    required this.data,
-    required this.token,
+    this.data,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
     return LoginResponse(
       status: json['status'],
       message: json['message'],
-      data: LoginData.fromJson(json['data']),
-      token: json['token'],
+      data: json['data'] != null
+          ? LoginData.fromJson(json['data'])
+          : null,
     );
   }
 }
